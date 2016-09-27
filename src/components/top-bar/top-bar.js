b5bdc59e43ae5cfe7e80d40a63f9c3ad8afdd787
itoonnn <itoonnn@gmail.com>
2016-09-27 09:08:43 +0700

    Try to apply theme at top-bar

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -2,36 +2,13 @@ import React from 'react';
 import { Nav, Navbar, NavItem, NavDropdown, FormControl, FormGroup, Button, MenuItem} from 'react-bootstrap';
 
 export default class TopBar extends React.Component{
+  
   render(){
+    const theme = require('../../styles/theme.css');
     return(
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Brand</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Navbar.Form pullLeft>
-              <FormGroup>
-                <FormControl type="text" placeholder="Search" />
-              </FormGroup>
-              {' '}
-              <Button type="submit">Submit</Button>
-            </Navbar.Form>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">Sign in</NavItem>
-              <NavDropdown eventKey={3} title="Cart" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <header id="header" className={theme.header-narrow} data-plugin-option='{"stickyEnabled": true, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": true, "stickyStartAt": 57, "stickySetTop": "-57px", "stickyChangeLogo": false}'>
+        </header>
       </div>
     )
   }
